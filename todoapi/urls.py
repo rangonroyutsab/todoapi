c640b5e861997b289e8289862a4db7067e820610
Rangon Roy Utsab <rangonroy.rzs17@gmail.com>
2026-07-15 16:20:55 +0600

    chore: removed duplicate url

--- todoapi/urls.py
+++ todoapi/urls.py
@@ -1,18 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
-
 
 BASE_URL = "api/v1/"
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path(f"{BASE_URL}login/", TokenObtainPairView.as_view(), name="login"),
-    path(f"{BASE_URL}token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path(f"{BASE_URL}", include("users.urls")),
     path(f"{BASE_URL}", include("tasks.urls")),
 ]
